11797437773674553e69520b8f49a1ab746a43ef
aliharby12 <aliharby565@gmail.com>
2020-08-22 00:53:52 -0700

    add pagination to the project

---
+++ resources/views/layouts/paginator.blade.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<div class="d-flex justify-content-satrt mb-5">
+  <div class="mb-5">
+    {{ $products->links() }}
+  </div>
+</div>
---
+++ resources/views/vendor/pagination/default.blade.php
@@ -0,0 +1,31 @@
+@if ($paginator->hasPages())
+        <nav class="navigation align-center">
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <a class="page-numbers bg-border-color current"><span>{{ $page }}</span></a>
+                        @else
+                            <a href="{{ $url }}" class="page-numbers bg-border-color"><span>{{ $page }}</span></a>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li>
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                </li>
+            @else
+                <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span aria-hidden="true">&rsaquo;</span>
+                </li>
+            @endif
+        </nav>
+
+@endif
